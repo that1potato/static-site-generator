28f321b98338fa4aaac5bc944905f7f335e9da82
Potato987. <55237095+that1potato@users.noreply.github.com>
2025-11-11 09:34:48 +1100

    add more test cases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -5,9 +5,32 @@ from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
+        node = TextNode('This is a text node', TextType.BOLD)
+        node2 = TextNode('This is a text node', TextType.BOLD)
         self.assertEqual(node, node2)
+    
+    def test_not_eq_0(self):
+        node = TextNode('This is a text node', TextType.BOLD)
+        node2 = TextNode('This is a text node', TextType.CODE)
+        self.assertNotEqual(node, node2)
+    
+    def test_not_eq_1(self):
+        node = TextNode('This is a text node', TextType.BOLD, url='https://www.example.com')
+        node2 = TextNode('This is a text node', TextType.BOLD)
+        self.assertNotEqual(node, node2)
+    
+    def test_not_eq_2(self):
+        node = TextNode('This is a text node.', TextType.BOLD)
+        node2 = TextNode('This is a text node', TextType.BOLD)
+        self.assertNotEqual(node, node2)
+    
+    def test_repr_0(self):
+        node = TextNode('test', TextType.BOLD, url='https://www.example.com')
+        self.assertEqual(repr(node), 'TextNode(test, bold, https://www.example.com)')
+    
+    def test_repr_1(self):
+        node = TextNode('test', TextType.ITALIC)
+        self.assertEqual(repr(node), 'TextNode(test, italic, None)')
 
 
 if __name__ == "__main__":
