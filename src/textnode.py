796c1a823dbcc829032c713117b1197d4dbdcc13
Potato987. <55237095+that1potato@users.noreply.github.com>
2025-11-16 09:25:21 +1100

    fix string repr for textnode

--- src/textnode.py
+++ src/textnode.py
@@ -10,17 +10,17 @@ class TextType(Enum):
     CODE = 'code'
     LINK = 'link'
     IMAGE = 'image'
-    
+
 class TextNode:
     def __init__(self, text: str, text_type: TextType, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
-    
+
     def __eq__(self, other):
         return self.text == other.text\
             and self.text_type == other.text_type\
             and self.url == other.url
-    
+
     def __repr__(self):
-        return f'TextNode({self.text}, {self.text_type.value}, {self.url if self.url else 'None'})'
+        return f'TextNode({self.text}, {self.text_type.value}, {self.url if self.url else "None"})'
