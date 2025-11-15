7c266ea05469d5e5a18f09d533721cba32fb380d
Potato987. <55237095+that1potato@users.noreply.github.com>
2025-11-15 17:21:07 +1100

    renamed files

--- src/test_util.py
+++ src/test_util.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from converter import text_node_to_html_node
+from util import text_node_to_html_node
 
 
 class TestTextNodeToHTMLNode(unittest.TestCase):
--- src/util.py
+++ src/util.py
@@ -1,16 +1,17 @@
-from textnode import TextNode, TextType
 from htmlnode import LeafNode
+from textnode import TextNode, TextType
+
 
 def text_node_to_html_node(text_node: TextNode):
-    '''
+    """
     convert a TextNode to an HTMLNode
-    '''
+    """
     if text_node.text_type == TextType.TEXT:
         return LeafNode(None, text_node.text)
     if text_node.text_type == TextType.BOLD:
-        return LeafNode('b', text_node.text)
+        return LeafNode("b", text_node.text)
     if text_node.text_type == TextType.ITALIC:
-        return LeafNode('i', text_node.text)
+        return LeafNode("i", text_node.text)
     if text_node.text_type == TextType.CODE:
         return LeafNode("code", text_node.text)
     if text_node.text_type == TextType.LINK:
@@ -18,3 +19,12 @@ def text_node_to_html_node(text_node: TextNode):
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
     raise ValueError(f"invalid text type: {text_node.text_type}")
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    """
+    takes a list of "old nodes", a delimiter, and a text type.
+    return a new list of nodes, where any "text" type nodes in
+    the input list are (potentially) split into multiple nodes based on the syntax
+    """
+    pass
