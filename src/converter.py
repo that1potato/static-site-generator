aaf89ef1834f16c753e8ebf174fe96673f65ad3a
Potato987. <55237095+that1potato@users.noreply.github.com>
2025-11-15 12:14:37 +1100

    create converter.py

---
+++ src/converter.py
@@ -0,0 +1,20 @@
+from textnode import TextNode, TextType
+from htmlnode import LeafNode
+
+def text_node_to_html_node(text_node: TextNode):
+    '''
+    convert a TextNode to an HTMLNode
+    '''
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode('b', text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode('i', text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"invalid text type: {text_node.text_type}")
