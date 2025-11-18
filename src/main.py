66d98432c6e0054ad608e72ca4632ed5403fcfc7
Potato987. <55237095+that1potato@users.noreply.github.com>
2025-11-18 21:07:56 +1100

    add copy

--- src/main.py
+++ src/main.py
@@ -1,4 +1,44 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
 
 node = TextNode('ur mom', TextType.BOLD)
 print(node)
+
+
+def copy(source, destination):
+	'''
+	copies all the contents from a source directory to a destination directory (in our case, static to public)
+	'''
+	if not os.path.isdir(source) or not os.path.isdir(destination):
+		print('not a directory')
+		return
+
+	# delete all files in detination
+	if os.path.exists(destination):
+		for filename in os.listdir(destination):
+			file_path = os.path.join(destination, filename)
+			if os.path.isdir(file_path):
+				shutil.rmtree(file_path)
+			else:
+				os.remove(file_path)
+			print(f"Path '{file_path}' deleted.")
+	# copy all files and subdirectories, nested files...
+	for root, _, files in os.walk(source):
+		# figure out where to put the copied items under destination
+		relative_path = os.path.relpath(root, source)
+		destination_root = (
+			os.path.join(destination, relative_path)
+			if relative_path != '.'
+			else destination
+		)
+		os.makedirs(destination_root, exist_ok=True)
+
+		for file_name in files:
+			source_path = os.path.join(root, file_name)
+			destination_path = os.path.join(destination_root, file_name)
+			shutil.copy2(source_path, destination_path)
+			# logging the path of each file copied for debugging
+			print(f"Copied '{source_path}' to '{destination_path}'")
+	return
