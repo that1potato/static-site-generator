92317f2d238b90c7cda24b9173aa54ae7e0d08e1
Potato987. <55237095+that1potato@users.noreply.github.com>
2025-11-18 14:47:41 +1100

    add block_to_block_type

--- src/block_util.py
+++ src/block_util.py
@@ -27,3 +27,30 @@ def markdown_to_blocks(markdown):
     parts = text.split("\n\n")
     blocks = [p.strip() for p in parts if p.strip()]
     return blocks
+
+
+def block_to_block_type(block):
+	lines = block.split("\n")
+
+	if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+		return BlockType.HEADING
+	if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+		return BlockType.CODE
+	if block.startswith(">"):
+		for line in lines:
+			if not line.startswith(">"):
+				return BlockType.PARAGRAPH
+		return BlockType.QUOTE
+	if block.startswith("- "):
+		for line in lines:
+			if not line.startswith("- "):
+				return BlockType.PARAGRAPH
+		return BlockType.UNORDERED_LIST
+	if block.startswith("1. "):
+		i = 1
+		for line in lines:
+			if not line.startswith(f"{i}. "):
+				return BlockType.PARAGRAPH
+			i += 1
+		return BlockType.ORDERED_LIST
+	return BlockType.PARAGRAPH
