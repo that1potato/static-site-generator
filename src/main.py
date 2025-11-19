663b36b4842873876229ee431ac2c2dd5021cdc7
Potato987. <55237095+that1potato@users.noreply.github.com>
2025-11-19 20:31:24 +1100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,6 +6,13 @@ from block_util import markdown_to_html_node
 from inline_util import extract_title
 
 
+static_path = './static'
+public_path = './public'
+content_path = './content'
+template_path = './template.html'
+default_base_path = '/'
+
+
 def copy(source, destination):
 	'''
 	copies all the contents from a source directory to a destination directory (in our case, static to public)
@@ -43,15 +50,7 @@ def copy(source, destination):
 	return
 
 
-def normalize_base_url(base_url):
-	if not base_url:
-		return '/'
-	if not base_url.endswith('/'):
-		base_url += '/'
-	return base_url
-
-
-def generate_page(base_url, from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
 	print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 	with open(from_path, "r", encoding="utf-8") as source_file:
 		markdown_content = source_file.read()
@@ -60,13 +59,11 @@ def generate_page(base_url, from_path, template_path, dest_path):
 
 	html_string = markdown_to_html_node(markdown_content).to_html()
 	title = extract_title(markdown_content)
-	output = (
-		template_content
-		.replace("{{ Title }}", title)
-		.replace("{{ Content }}", html_string)
-		.replace('href="/', f'href="{base_url}')
-		.replace('src="/', f'src="{base_url}')
-	)
+	output = template_content\
+		.replace("{{ Title }}", title)\
+		.replace("{{ Content }}", html_string)\
+		.replace('href="/', 'href="' + base_path)\
+		.replace('src="/', 'src="' + base_path)
 
 	dest_dir = os.path.dirname(dest_path)
 	if dest_dir:
@@ -75,7 +72,7 @@ def generate_page(base_url, from_path, template_path, dest_path):
 		dest_file.write(output)
 
 
-def generate_pages_recursive(base_url, content_dir, template_path, dest_dir):
+def generate_pages_recursive(content_dir, template_path, dest_dir, base_path):
 	if not os.path.isdir(content_dir):
 		print(f"Content directory '{content_dir}' does not exist.")
 		return
@@ -88,23 +85,19 @@ def generate_pages_recursive(base_url, content_dir, template_path, dest_dir):
 			destination_root = dest_dir if relative_root == "." else os.path.join(dest_dir, relative_root)
 			destination_name = os.path.splitext(file_name)[0] + ".html"
 			destination_path = os.path.join(destination_root, destination_name)
-			generate_page(base_url, source_path, template_path, destination_path)
+			generate_page(source_path, template_path, destination_path, base_path)
 
 
 def main():
+	base_path = default_base_path
 	if len(sys.argv) > 1:
-		base_path = os.path.abspath(sys.argv[1])
-		base_url = normalize_base_url(sys.argv[2] if len(sys.argv) > 2 else '/')
-	else:
-		# default to the project root (parent directory of this file's folder)
-		base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-		base_url = normalize_base_url('/')
-	static_path = os.path.join(base_path, 'static')
-	docs_path = os.path.join(base_path, 'docs')
-	content_path = os.path.join(base_path, 'content')
-	template_path = os.path.join(base_path, 'template.html')
-	copy(static_path, docs_path)
-	generate_pages_recursive(base_url, content_path, template_path, docs_path)
+		base_path = sys.argv[1]
+
+	print('Copying static files to public directory...')
+	copy(static_path, public_path)
+
+	print('Generating content...')
+	generate_pages_recursive(content_path, template_path, public_path, base_path)
 
 
 if __name__ == '__main__':
